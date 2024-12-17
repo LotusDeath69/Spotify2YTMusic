c43e91520f2e74355b6dd046664a43f0a7720c0f
Tri H <lotusdeath69@gmail.com>
2024-12-17 00:41:08 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from dotenv import load_env
+import os
+load_env() 
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+
+
+result = requests.get()
+
+
+class Handler: 
+    
+    def __init__(self):
+        pass
+    
+    
+    def getAPItoken(self):
+        pass 
